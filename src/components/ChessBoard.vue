152984efad9b288d3c00686aa78ce06aab7acca6
hugocowan <hugocowan915@gmail.com>
2021-12-05 22:27:50 +0000

    Added row and column letters/numbers.

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -3,8 +3,12 @@
     <div
 		v-for="square in boardSquares" 
 		v-bind:key="square.index" 
-		:class="'square ' + square.colour"
-    />
+		:class="`square ${square.colour} ${square.code}${square.index}`"
+	>
+		<span v-if="square.index === 1 || square.code === 'A'" >
+			{{ (square.code === 'A' && square.index === 1) ? `${square.code}${square.index}` : (square.code === 'A') ? square.index : square.code }}
+		</span> 
+	</div>
   </div>
 </template>
 
@@ -22,25 +26,35 @@ import { Options, Vue } from "vue-class-component";
 	},
 })
 export default class ChessBoard extends Vue {
-	private getSquares = (): { colour: string; index: number; }[] => {
+
+	// Make an array of squares including their colour and code.
+	private getSquares = (): { colour: string; index: number; code: string }[] => {
 		let boardSquares = [],
 			colourA = "white",
 			colourB = "black",
-			oldcolourA = null;
+			oldcolourA = null,
+			columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+			currentColumn = 'A',
+			currentIndex = 8;
 
 		for (let i = 0; i < 64; i++) {
 			if (i > 0 && i % 8 === 0) {
 				(oldcolourA = colourA), (colourA = colourB), (colourB = oldcolourA);
+				currentIndex = currentIndex === 1 ?  8 : --currentIndex;
 			}
 
 			boardSquares[i] = {
 				colour: i % 2 === 0 ? colourA : colourB,
-				index: i,
+				index: currentIndex,
+				code: `${currentColumn}`
 			};
+
+			currentColumn = columns[columns.indexOf(currentColumn) + 1] ?? 'A';
 		}
 
 		return boardSquares;
 	};
+
 	private boardSquares = this.getSquares();
 }
 </script>
@@ -50,14 +64,7 @@ export default class ChessBoard extends Vue {
 h3 {
 	margin: 40px 0 0;
 }
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
+
 a {
 	color: #42b983;
 }
@@ -75,10 +82,26 @@ a {
 
 		&.white {
 			background: rgb(233, 200, 138);
+
+			span {
+				color: rgb(117, 60, 13);
+			}
 		}
 
 		&.black {
 			background: rgb(117, 60, 13);
+
+			span {
+				color: rgb(233, 200, 138);
+			}
+		}
+
+		span {
+			position: relative;
+			top: 75%;
+			right: 35%;
+			font-size: 11px;
+			padding-right: 5px;
 		}
 	}
 }
