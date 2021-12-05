e3e57efef9129cbada52d4ef519dad7f15ff120c
hugocowan <hugocowan915@gmail.com>
2021-12-05 21:48:12 +0000

    Edited key files to use tabs instead of spaces

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    <ChessBoard msg="Welcome to Your Vue.js + TypeScript App" :wassup="wassup"/>
-  </div>
+	<div class="home">
+		<ChessBoard msg="Welcome to Your Vue.js + TypeScript App" :wassup="wassup"/>
+	</div>
 </template>
 
 <script lang="ts">
@@ -10,9 +10,9 @@ import { Options, Vue } from 'vue-class-component';
 import ChessBoard from '@/components/ChessBoard.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    ChessBoard,
-  }
+	components: {
+		ChessBoard,
+	}
 })
 export default class Home extends Vue {}
 </script>
